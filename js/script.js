ea4a505eac7467104f886fa1ab0356a6236c0bb6
hongngat <48482193+hongngat@users.noreply.github.com>
2020-04-23 16:06:00 +0700

    a

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-var myIndex = 0;
-carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2500);    
-}
+$(document).ready(function(){ 
+	var touch 	= $('#resp-menu');
+	var menu 	= $('#navMenu');
+ 
+	$(touch).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+	
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 767 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+	
+});
